12de7dbd6cff885cfe9bf4760ecaf17b25ed775a
Vladimir Ivanov <ivvl82@gmail.com>
2019-11-26 14:21:11 +0800

    Re-write lemmas concerned to the size of 'FileSystem'.

--- src/FileSystem.v
+++ src/FileSystem.v
@@ -1,5 +1,6 @@
 Set Implicit Arguments.
 Require Import Arith.PeanoNat.
+Require Import ArithRing.
 Require Import FunInd.
 Require Import Init.Datatypes.
 Require Import Lists.List.
@@ -102,19 +103,37 @@ Section FSLevel.
       apply injective_projections; reflexivity.
   Qed.
 
-  Lemma node_split_level_l : forall (x h:Node Name Tag) (t:FileSystem Name Tag),
+  Lemma split_l_split : forall (fs l r:FileSystem Name Tag),
+      (l, r) = fs_split fs -> (l, nil) = fs_split l.
+  Proof.
+    intro fs. induction fs; intros l r H.
+    - inversion H. unfold fs_split. reflexivity.
+    - remember (fs_split fs) as p. rewrite Heqp in IHfs.
+      pattern p in Heqp. rewrite surjective_pairing in Heqp.
+      specialize (IHfs (fst p) (snd p) Heqp).
+      remember (node_split a) as q. pattern q in Heqq.
+      rewrite surjective_pairing in Heqq.
+      rewrite <- (split_cons Heqq Heqp) in H. inversion H.
+      assert ((fst q, nil) = node_split (fst q)).
+      + destruct a; simpl in Heqq; inversion Heqq; rewrite H3;
+          apply injective_projections; simpl; assumption || reflexivity.
+      + rewrite <- (split_cons H0 IHfs). apply injective_projections; auto.
+  Qed.
+  Arguments split_l_split {_ _ _}.
+
+  Lemma node_split_l_level : forall (x h:Node Name Tag) (t:FileSystem Name Tag),
       (h, t) = node_split x -> node_level h = 1.
   Proof.
     intros. destruct x; simpl in H; inversion H; reflexivity.
   Qed.
-  Arguments node_split_level_l {_ _ _}.
+  Arguments node_split_l_level {_ _ _}.
 
-  Lemma node_split_level_r : forall (x h:Node Name Tag) (t:FileSystem Name Tag),
+  Lemma node_split_r_level : forall (x h:Node Name Tag) (t:FileSystem Name Tag),
       (h, t) = node_split x -> S (fs_level t) = node_level x.
   Proof.
     intros. destruct x; simpl in *; inversion H; reflexivity.
   Qed.
-  Arguments node_split_level_r {_ _ _}.
+  Arguments node_split_r_level {_ _ _}.
 
   Lemma fs_split_level : forall (fs l r:FileSystem Name Tag),
       nil <> fs -> (l, r) = fs_split fs ->
@@ -125,7 +144,7 @@ Section FSLevel.
     - simpl. remember (node_split n) as p. pattern p in Heqp.
       rewrite surjective_pairing in Heqp. rewrite <- (split_eq Heqp) in H0.
       inversion H0. simpl. do 2 rewrite Nat.max_0_r.
-      rewrite (node_split_level_l Heqp). rewrite <- (node_split_level_r Heqp).
+      rewrite (node_split_l_level Heqp). rewrite <- (node_split_r_level Heqp).
       simpl. reflexivity.
     - remember (fs_split (a :: fs)) as p. pattern p in Heqp.
       rewrite surjective_pairing in Heqp. specialize (IHfs a (fst p) (snd p)).
@@ -135,8 +154,8 @@ Section FSLevel.
       rewrite <- H2 in H0. inversion H0. rewrite level_cons. rewrite IHfs.
       rewrite level_concat. rewrite Nat.succ_max_distr.
       do 2 rewrite Nat.max_assoc. f_equal.
-      rewrite <- (node_split_level_r Heqq). rewrite Nat.max_comm. f_equal.
-      rewrite level_cons. rewrite (node_split_level_l Heqq).
+      rewrite <- (node_split_r_level Heqq). rewrite Nat.max_comm. f_equal.
+      rewrite level_cons. rewrite (node_split_l_level Heqq).
       symmetry. apply Nat.max_r_iff. destruct (fst p).
       + unfold fs_split in Heqp. simpl in Heqp. inversion Heqp.
       + assert (nil <> n0 :: f). 1:discriminate. apply (fs_level_gt_1 H3).
@@ -156,4 +175,90 @@ Section FSLevel.
     intros. assert (nil <> n :: l). 1:apply nil_cons. symmetry in teq0.
     rewrite (fs_split_level H teq0). apply Nat.max_lt_iff. right. auto.
   Qed.
+
+  Lemma fold_level_nil : forall (A:Type) (f:Node Name Tag->A->A) (a0:A),
+      fold_level f a0 nil = a0.
+  Proof.
+    intros. remember nil as fs. functional induction (fold_level f a0 fs).
+    1:reflexivity. inversion e0. symmetry in H1. specialize (IHa H1).
+    rewrite H1 in IHa. rewrite IHa. reflexivity.
+  Qed.
+
+  Lemma split_fold_level :
+    forall (A:Type) (f:Node Name Tag->A->A) (a0:A) (fs l r:FileSystem Name Tag),
+      (l, r) = fs_split fs ->
+      fold_level f a0 fs = fold_right f (fold_level f a0 r) l.
+  Proof.
+    intros A f a0 fs. functional induction (fold_level f a0 fs); intros.
+    - inversion H. rewrite fold_level_nil. reflexivity.
+    - rewrite e0 in H. inversion H. reflexivity.
+  Qed.
+  Arguments split_fold_level {_} _ _ {_ _ _}.
+
+  Lemma split_l_fold_level :
+    forall (A:Type) (f:Node Name Tag->A->A) (a0:A) (fs l r:FileSystem Name Tag),
+      (l, r) = fs_split fs -> fold_level f a0 l = fold_right f a0 l.
+  Proof.
+    intros A f a0 fs. functional induction (fold_level f a0 fs); intros l' r' H.
+    - inversion H. rewrite fold_level_nil. reflexivity.
+    - rewrite e0 in H. inversion H. symmetry in e0. pose proof (split_l_split e0).
+      rewrite (split_fold_level f a0 H0). rewrite fold_level_nil. reflexivity.
+  Qed.
+  Arguments split_l_fold_level {_} _ _ {_ _ _}.
 End FSLevel.
+
+Section FSTree.
+  Variables Name Tag : Type.
+
+  Definition size (fs:FileSystem Name Tag) : nat := fold_level (fun _ => S) O fs.
+
+  Lemma split_size : forall (fs l r:FileSystem Name Tag),
+      (l, r) = fs_split fs -> size fs = size l + size r.
+  Proof.
+    intros fs. unfold size.
+    functional induction (fold_level (fun _ => S) O fs); intros l' r' H.
+    - unfold fs_split in H. simpl in H. inversion H. rewrite fold_level_nil.
+      reflexivity.
+    - rewrite e0 in H. inversion H.
+      remember (fold_level (fun _ => S) 0 r) as n.
+      symmetry in e0. rewrite (split_l_fold_level (fun _ => S) 0 fs e0).
+      clear IHs H H1 H2 Heqn e0 fs l' r r' y. induction l. 1:reflexivity.
+      simpl. rewrite IHl. reflexivity.
+  Qed.
+  Arguments split_size {_ _ _}.
+
+  Lemma split_l_size : forall (fs l r:FileSystem Name Tag),
+      (l, r) = fs_split fs -> size l = length l.
+  Proof.
+    intros. unfold size. rewrite (split_l_fold_level (fun _ => S) 0 fs H).
+    clear H fs r. induction l. 1:reflexivity. simpl. rewrite IHl. reflexivity.
+  Qed.
+  Arguments split_l_size {_ _ _}.
+
+  Lemma size_ge_0 : forall (fs:FileSystem Name Tag), 0 <= size fs.
+  Proof.
+    intro fs. functional induction (fold_level (fun _ => S) O fs).
+    - unfold size. rewrite fold_level_nil. trivial.
+    - symmetry in e0. rewrite (split_size e0). pattern 0. rewrite <- Nat.add_0_r.
+      apply Nat.add_le_mono.
+      + rewrite (split_l_size e0). clear e0 IHs fs r y.
+        induction l. 1:reflexivity. simpl. revert IHl. apply Nat.le_le_succ_r.
+      + assumption.
+  Qed.
+
+  Lemma size_concat : forall (fs fs':FileSystem Name Tag),
+      size (fs ++ fs') = size fs + size fs'.
+  Proof.
+    intro fs. functional induction (fold_level (fun _ => S) O fs); intro fs'.
+    - rewrite app_nil_l. unfold size. rewrite fold_level_nil. trivial.
+    - remember (fs_split fs') as p. pattern p in Heqp.
+      rewrite surjective_pairing in Heqp. symmetry in e0.
+      pose proof (split_concat fs fs' e0 Heqp). rewrite (split_size H).
+      rewrite (IHs (snd p)). rewrite (split_size Heqp).
+      do 2 rewrite Nat.add_assoc. apply Nat.add_cancel_r.
+      rewrite (split_size e0). cut (size (l ++ fst p) = size l + size (fst p)).
+      + intro. rewrite H0. ring.
+      + rewrite (split_l_size H). rewrite (split_l_size e0).
+        rewrite (split_l_size Heqp). apply app_length.
+  Qed.
+End FSTree.
